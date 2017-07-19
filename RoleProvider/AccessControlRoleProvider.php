d5ab9a193aea4637fc5982638633ff98547abac8
Stephan Wentz <swentz@brainbits.net>
2017-07-19 18:51:18 +0200

    subsplit fixes, move bundle files from component to bundle

---
+++ RoleProvider/AccessControlRoleProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the phlexible package.
+ *
+ * (c) Stephan Wentz <sw@brainbits.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Phlexible\Bundle\AccessControlBundle\RoleProvider;
+
+use Phlexible\Bundle\GuiBundle\Security\RoleProvider\RoleProvider;
+
+/**
+ * Access control role provider.
+ *
+ * @author Stephan Wentz <sw@brainbits.net>
+ */
+class AccessControlRoleProvider extends RoleProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function provideRoles()
+    {
+        return array(
+            'ROLE_ACCESS_CONTROL',
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function exposeRoles()
+    {
+        return $this->provideRoles();
+    }
+}
